6c5cd6feb339c372f04087cb6026a04d8aef0532
Timofey Bogdanov <timofeybogdanov@Timofeys-iMac.local>
2020-06-08 12:23:08 -0700

    complete task 2, establish state with useState hooks

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import "./App.css";
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 
+  const { homeScore, awayScore } = useState()
+
   return (
     <div className="container">
       <section className="scoreboard">
